2cdc34b062cdfd2c7af46b0c42c5f82f87935161
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-04-17 15:36:56 -0400

    Finalized battery on homescreen.

--- res/js/homedata.js
+++ res/js/homedata.js
@@ -1,16 +1,31 @@
 function updateSysData() {
 
-    $.getJSON('/si/bat/per', (json) => {
-        document.getElementById('bat-percent').innerHTML = json.toString()
+    $.getJSON('/si/bat/acc', (json) => {
+        
+        const statmap = new Map(); statmap.set('true', true); statmap.set('false', false)
+        const charging = statmap.get(json.toString())
+
+        $.getJSON('/si/bat/per', (json) => {
+            const percent = json.toString()
+            const text = document.getElementById('bat-percent')
+            const image = document.getElementById('bat-image')
+
+            text.innerHTML = percent
+
+            if (charging) {
+                if (parseInt(percent) >= 100) image.setAttribute('src', '/res/img/bat-charging.svg')
+                else image.setAttribute('src', '/res/img/bat-charged.svg')
+            } else {
+                if (parseInt(percent) <= 25) image.setAttribute('src', '/res/img/bat-1.svg')
+                else if (parseInt(percent) > 25 && parseInt(percent) <= 50) image.setAttribute('src', '/res/img/bat-2.svg')
+                else if (parseInt(percent) > 50 && parseInt(percent) <= 75) image.setAttribute('src', '/res/img/bat-3.svg')
+                else if (parseInt(percent) > 75) image.setAttribute('src', '/res/img/bat-45.svg')
+            }
+
+        })
+
     })
 
-    $.getJSON('/si/bat/acc', (json) => {
-        if (json.toString() == 'true') {
-            document.getElementById('bat-charging').innerHTML = 'Currently'
-        } else {
-            document.getElementById('bat-charging').innerHTML = 'Not'
-        }
-    })
 
     setTimeout(updateSysData, 5000)
 
